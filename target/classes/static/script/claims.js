982259c66088e2f3f8a8ba99e89ff6a43bc2ab6c
eSuminski <rctolinski@gmail.com>
2021-07-01 09:19:18 -0700

    added create manager page, need to implement create manager function and add link to create manager page on login page

--- target/classes/static/script/claims.js
+++ target/classes/static/script/claims.js
@@ -13,25 +13,28 @@ const claimTableBody = document.getElementById("claimTableBody");
 const claimButton = document.getElementById("claimButton");
 
 async function sendClaim() {
-    const path = settings.server + "/claims";
-    const claim = document.getElementById("claim");
-    const amount = document.getElementById("amount");
-    
-    const req = {
-        claim: claim.value,
-        amount: amount.value,
-    };
-    const config = {
-        method: "POST",
-        headers: {
-            "Content-Type": "application/json",
-            "id": userId,
-        },
-        body: JSON.stringify(req),
-    };
+    const result = confirm("Are you sure you want to submit this claim?");
+    if(result){
+        const path = settings.server + "/claims";
+        const claim = document.getElementById("claim");
+        const amount = document.getElementById("amount");
+        
+        const req = {
+            claim: claim.value,
+            amount: amount.value,
+        };
+        const config = {
+            method: "POST",
+            headers: {
+                "Content-Type": "application/json",
+                "id": userId,
+            },
+            body: JSON.stringify(req),
+        };
 
-    const resp = await fetch(path, config);
-    getClaims();
+        const resp = await fetch(path, config);
+        getClaims();
+    }
 };
 
 async function getClaims() {
@@ -51,9 +54,9 @@ async function getClaims() {
     let rows = "";
     for (let claim of claims) {
         rows += `<tr>
-            <td>${claim.claim}</td>
+            <td>${claim.reason}</td>
             <td>${claim.amount}</td>
-            <td><i class="${icon(claim.approved)}"></i></td>
+            <td><i class="${icon(claim.status)}"></i></td>
             </tr>`
     };
     claimTableBody.innerHTML = rows;
@@ -61,9 +64,9 @@ async function getClaims() {
 
 function icon(approved) {
     console.log(approved)
-    if (approved === true) {
+    if (approved === "Accepted") {
         return "fa fa-check"
-    } else if (approved === false) {
+    } else if (approved === "Rejected") {
         return "fa fa-remove"
     }
 }
